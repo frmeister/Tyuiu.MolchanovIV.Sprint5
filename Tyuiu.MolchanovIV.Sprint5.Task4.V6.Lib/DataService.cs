8e2b6896ef5d2d68855c74690c421d46d114cfe2
frmeister <frmeister1337@gmail.com>
2025-11-29 10:29:20 +0500

    Task4 - new

---
+++ Tyuiu.MolchanovIV.Sprint5.Task4.V6.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MolchanovIV.Sprint5Task4.V6.Lib
+{
+    public class DataService : ISprint5Task4V6
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double input, res;
+            string inputStr = File.ReadAllText(path);
+
+            input = double.Parse(inputStr, CultureInfo.InvariantCulture);
+
+            res = Math.Round((1 / Math.Cos(input)) + 2.2 * Math.Pow(input, 2), 3);
+
+            return res;
+        }
+    }
+}
